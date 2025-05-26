79c1304a592c42719248a250abc3a9e0b4c8d562
Aiden Robinson <aiden.robinson1@gmail.com>
2025-05-26 13:55:44 -0400

    design hashset solved

---
+++ Linked List/p705_design_hashset.py
@@ -0,0 +1,55 @@
+class ListNode:
+    def __init__(self,key):
+        self.val= key
+        self.next= None
+
+class MyHashSet:
+
+    def __init__(self):
+        self.set= [ListNode(0) for i in range(10**4)]
+
+        
+
+    def add(self, key: int) -> None:
+        index= key % len(self.set)
+        cur=self.set[index]
+
+        while cur.next:
+            if cur.next.val== key: #detects duplicates
+                return
+            cur=cur.next
+        
+        cur.next= ListNode(key)
+        
+
+    def remove(self, key: int) -> None:
+        index= key % len(self.set)
+        cur=self.set[index]
+
+        while cur.next:
+            if cur.next.val== key: #detects if key exists
+                cur.next= cur.next.next
+                return
+            cur=cur.next
+        
+       
+
+    def contains(self, key: int) -> bool:
+        index= key % len(self.set)
+        cur=self.set[index]
+
+        while cur.next:
+            if cur.next.val== key: #detects if key exits
+                return True
+            cur=cur.next
+
+        return False
+
+#time complexity O(1)
+#space complexity O(n)
+
+# Your MyHashSet object will be instantiated and called as such:
+# obj = MyHashSet()
+# obj.add(key)
+# obj.remove(key)
+# param_3 = obj.contains(key)
