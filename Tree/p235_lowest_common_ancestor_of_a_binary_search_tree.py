bfa56c411e6384dd8dbfba14e8e1ee5b68fae56b
Aiden Robinson <aiden.robinson1@gmail.com>
2025-07-12 11:49:59 -0400

    lowest common ancestor of a binary search tree

---
+++ Tree/p235_lowest_common_ancestor_of_a_binary_search_tree.py
@@ -0,0 +1,25 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        cur = root
+
+
+        while cur:
+            if cur.val>p.val and cur.val>q.val:
+                cur=cur.left
+
+            elif cur.val<p.val and cur.val<q.val:
+                cur=cur.right
+            
+            else:
+                return cur
+        
+
+#time complexity O(logN) best cause cause we cut half tree each time, O(N) worse case if its a linked list
+#space complexity O(1)
