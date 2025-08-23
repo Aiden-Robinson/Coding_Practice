fbc6f05e6a8a0cdc1ff27bfe4df9471226e09dcf
Aiden Robinson <aiden.robinson1@gmail.com>
2025-08-23 11:48:22 -0400

    jump game

---
+++ Greedy/p55_jump_game.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+     
+        goal= len(nums)-1
+        for i in range(len(nums)-1, -1, -1):
+            if i + nums[i] >= goal:
+                goal= i
+        
+
+        if goal==0:
+            return True
+        
+        return False
+            
+#time complexity O(N)
+#space complexity O(1)
+            
+
+
+
