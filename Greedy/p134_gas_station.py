be5e872bd6c6c513f5c29310e43987adbfdf5d19
Aiden Robinson <aiden.robinson1@gmail.com>
2025-08-26 10:27:50 -0400

    gas station

---
+++ Greedy/p134_gas_station.py
@@ -0,0 +1,24 @@
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+        if sum(gas)<sum(cost):
+            return -1
+
+        start=0
+        total=0
+        for i in range(len(gas)):
+            diff= gas[i]-cost[i]
+
+            total+=diff
+
+            if total<0:
+                total=0
+                start=i+1
+        
+        return start
+
+#time complexity O(N)
+#space complexity O(1)
+
+
+        
