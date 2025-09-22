748bbde3222ad152f3f3abb349d06485ecf8a7f3
Aiden Robinson <aiden.robinson1@gmail.com>
2025-09-22 11:42:02 -0400

    rotate image

---
+++ Array/p48_rotate_image.py
@@ -0,0 +1,29 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
+        l,r= 0, len(matrix)-1
+
+        while l<r:
+            top,bottom=l,r #can do this because its a nxn
+            for i in range(r-l): 
+                topleft=matrix[top][l+i]
+
+                #move bottom left to top left
+                matrix[top][l+i]=matrix[bottom-i][l]
+
+                #move bottom right to bottom left
+                matrix[bottom-i][l]= matrix[bottom][r-i]
+
+                #move top right to bottom right
+                matrix[bottom][r-i]= matrix[top+i][r]
+
+                #move top left to top right
+                matrix[top+i][r]= topleft
+            
+            l+=1
+            r-=1
+    
+    #time complexity O(N^2)
+    #space complexity O(1)
