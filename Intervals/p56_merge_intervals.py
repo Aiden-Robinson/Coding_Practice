23c72d2ffa3c572315e335fb3250964d5a9ba56f
Aiden Robinson <aiden.robinson1@gmail.com>
2025-10-06 21:02:49 -0400

    merge intervals

--- Intervals/p56_merge_intervals.py
+++ Intervals/p56_merge_intervals.py
@@ -1,33 +1,29 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-
-        intervals.sort(key=lambda x: x[0])
+        intervals.sort(key= lambda x: x[0])
+        print(intervals)
         res=[]
-        
+
         i=0
-        
-        while i< len(intervals):
-            lowBound= intervals[i][0]
-            highBound= intervals[i][1]
+        while i< (len(intervals)):
 
-            while i<len(intervals) -1 and highBound>=intervals[i+1][0]:
-                highBound= max(highBound,intervals[i+1][1])
+            lowBound=intervals[i][0]
+            highBound=intervals[i][1]
+            while i+1<len(intervals) and highBound>=intervals[i+1][0]:
+                highBound=max(highBound, intervals[i+1][1])
                 i+=1
-
             i+=1
+            
+            res.append([lowBound,highBound])
 
-            newInterval= [lowBound,highBound]
-            res.append(newInterval)
-
-
+        
         return res
+
+
+   
             
 #time complexity O(NLogN), from the sorting
 #space complexity O(N)
 
 
             
-
-            
-
-
