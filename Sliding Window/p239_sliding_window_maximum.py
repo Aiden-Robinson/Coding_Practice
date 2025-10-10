90f9e6c0b94b81d74ca4f110f243b4f6a1b3bd5e
Aiden Robinson <aiden.robinson1@gmail.com>
2025-10-10 14:27:03 -0400

    sliding window maximum

---
+++ Sliding Window/p239_sliding_window_maximum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output=[]
+
+        l,r=0,0
+        q= collections.deque()
+
+        while r < len(nums):
+
+            while q and (nums[q[-1]]<nums[r]):
+                q.pop()
+            q.append(r)
+
+            if q[0]<l:
+                q.popleft()
+
+            if (r+1)>=k:
+                output.append(nums[q[0]])
+                l+=1
+        
+            r+=1
+        
+        return output
+#time complexity O(N)
+#space complexity O(N)
