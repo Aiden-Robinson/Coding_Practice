a6051ebbdd00e1c1e7c01c3ef3ba216fb194a891
Aiden Robinson <aiden.robinson1@gmail.com>
2025-10-29 13:11:44 -0400

    fobonacci number

---
+++ Dynamic Programming/p509_fibonacci_number.py
@@ -0,0 +1,12 @@
+class Solution:
+    def fib(self, n: int) -> int:
+
+        dp=[0,1]
+
+        for i in range(2,n+1):
+            dp.append(dp[-1]+dp[-2])
+        
+        return dp[n]
+        
+    #time complexity O(N)
+    #space complexity O(N)
