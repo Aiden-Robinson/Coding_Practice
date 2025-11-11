fea4707691354425264c7dcf99028e07e67504b4
Aiden Robinson <aiden.robinson1@gmail.com>
2025-11-11 11:22:52 -0500

    linked list cycle II

---
+++ Linked List/p142_linked_list_cycle_II.py
@@ -0,0 +1,31 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def detectCycle(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        
+        slow= head
+        fast= head
+
+        while fast and fast.next:
+            fast= fast.next.next
+            slow=slow.next
+            if slow==fast:
+                pointer= head #find distance from start up until cycle
+                while pointer!=slow:
+                    pointer=pointer.next
+                    slow=slow.next
+                return pointer
+
+            
+           
+        
+        return None
+
+#time complexity O(N)
+#space complexity O(1)
