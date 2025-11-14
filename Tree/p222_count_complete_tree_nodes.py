81cf74113d8dcd888bc49dba51530fe3cd331c14
Aiden Robinson <aiden.robinson1@gmail.com>
2025-11-14 18:02:13 -0500

    count compelte tree nodes

---
+++ Tree/p222_count_complete_tree_nodes.py
@@ -0,0 +1,37 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countNodes(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return 0
+
+        def getLeftHeight(node):
+            h=0
+            while node:
+                h+=1
+                node=node.left
+            return h
+        
+        def getRightHeight(node):
+            h=0
+            while node:
+                h+=1
+                node=node.right
+            return h
+        
+        lh= getLeftHeight(root)
+        rh=getRightHeight(root)
+
+        if lh==rh:
+            return 2**lh-1
+        
+        return 1+ self.countNodes(root.left) + self.countNodes(root.right)
+    
+    #time complexity O((logn)^2)
+    #space complexity O(logn)
+    
