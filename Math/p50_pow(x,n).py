7a912785576c58f4ab99494769a0f89f6b4a680c
Aiden Robinson <aiden.robinson1@gmail.com>
2025-11-21 12:03:43 -0500

    pow(x,n)

---
+++ Math/p50_pow(x,n).py
@@ -0,0 +1,22 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        
+        def helper(x,n):
+            if x==0:
+                return 0
+            if n==0:
+                return 1
+            
+            
+            res=helper(x,n//2)
+            res=res*res
+            
+            return x*res if n%2==1 else res
+
+
+        res=helper(x,abs(n))
+
+        return res if n>=0 else 1/res #handes if its a negative exponent
+
+#time complexity O(log n)
+#space complexity O(log n)
