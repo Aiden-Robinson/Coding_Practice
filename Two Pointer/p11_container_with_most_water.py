6c83163c19be8b4c1a1c39741d322fa49845d89a
Aiden Robinson <aiden.robinson1@gmail.com>
2025-11-22 13:01:00 -0500

    container with most water redone

--- Two Pointer/p11_container_with_most_water.py
+++ Two Pointer/p11_container_with_most_water.py
@@ -1,31 +1,24 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-
-    
-        # store the distance from first line to line itself as w
-        #take the minumum height of any two vertical lines
-
-        def getArea(ind1, ind2)->int: #O(N)
-            w=abs(ind1-ind2)
-            h=min(height[ind1], height[ind2])
-            return w*h
-        
-
         l=0
         r=len(height)-1
-        maxArea=0
+        res=0
 
-        while l<r:
+        def getArea(l,r):
+            width= r-l
+            length= min(height[l], height[r])
+            return width*length
+        
+        while r>=l:
             area= getArea(l,r)
-            maxArea=max(maxArea,area)
+            res= max(res,area)
+            print(l,r,area)
 
-            if height[l] < height[r]:
+            if height[l]<=height[r]:
                 l+=1
             else:
                 r-=1
-        
-        return maxArea
-            
+        return res
 
 #time complexity O(N)
 #space complexity O(1)
