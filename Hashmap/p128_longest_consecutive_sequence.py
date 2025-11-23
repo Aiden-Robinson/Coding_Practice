c5fbc64292a93133d3e5db1c9de53d5ed2503bba
Aiden Robinson <aiden.robinson1@gmail.com>
2025-11-23 16:46:50 -0500

    longest consecutive sequence

--- Hashmap/p128_longest_consecutive_sequence.py
+++ Hashmap/p128_longest_consecutive_sequence.py
@@ -1,19 +1,22 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
-        seen=set()
+        #put everything in a set
+        #iterate through again, run each number into the ground checking if its in the set
+
         res=0
-        
+        seen=set()
+
         for num in nums:
             seen.add(num)
-
+        
         for num in seen:
-            if (num-1) not in seen: #denoting it is the start of a sequence
-                length=0
-
-                while num+length in seen:
-                    length+=1
-                    res= max(res, length)
+            if (num-1) not in seen: #only check it if its the smallest in sequence
+                window=0
+                while num+window in seen:
+                    window+=1
+                    res=max(res,window)
         
         return res
+
 #time complexity O(N)
 #space complexity O(N)
