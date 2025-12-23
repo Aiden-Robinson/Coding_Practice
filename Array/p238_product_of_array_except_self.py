95babe6cc5d0d8a4ed6ac0e840b9cd6da23e5036
Aiden Robinson <aiden.robinson1@gmail.com>
2025-12-23 13:42:44 -0500

    product of array except itself

--- Array/p238_product_of_array_except_self.py
+++ Array/p238_product_of_array_except_self.py
@@ -1,21 +1,21 @@
+
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
         
-        n=len(nums)
-        answer=[1]*n
+        #values to the left
+        prefix=[1]*len(nums)
+        for i in range(1,len(nums)):
+            prefix[i]*=nums[i-1]*prefix[i-1]
 
-        prefix=1
-
-        for i in range(n):
-            answer[i]=prefix
-            prefix*=nums[i]
-
-        postfix=1
-        for i in range(n-1, -1, -1):
-            answer[i]*=postfix
-            postfix*=nums[i]
-
-        return answer
+        #values to the right
+        postfix=[1]*len(nums)
+        for i in range(len(nums)-2,-1,-1):
+            postfix[i]*=nums[i+1]*postfix[i+1]
+       
+        for i in range(len(postfix)):
+            postfix[i]*=prefix[i]
         
+        return postfix
+
 #time complexity O(N)
 #space complexity O(1), the result array is not part of the memory in this problem
