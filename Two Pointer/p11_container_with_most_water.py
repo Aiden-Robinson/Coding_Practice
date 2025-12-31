28d2d7f02e74e47bd059058323f9bb345818685b
Aiden Robinson <aiden.robinson1@gmail.com>
2025-12-31 12:35:01 -0800

    Refactor maxArea method in p11_container_with_most_water
    Refactor maxArea method to simplify area calculation and improve readability.

--- Two Pointer/p11_container_with_most_water.py
+++ Two Pointer/p11_container_with_most_water.py
@@ -1,24 +1,23 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
+        if not height:
+            return 0
+        
+        res=0
         l=0
         r=len(height)-1
-        res=0
-
         def getArea(l,r):
-            width= r-l
-            length= min(height[l], height[r])
-            return width*length
-        
-        while r>=l:
-            area= getArea(l,r)
-            res= max(res,area)
-            print(l,r,area)
-
+            return min(height[l],height[r])*(r-l)
+        while l<r:
+            res= max(res,getArea(l,r))
             if height[l]<=height[r]:
                 l+=1
             else:
                 r-=1
+      
+
         return res
+
 
 #time complexity O(N)
 #space complexity O(1)
