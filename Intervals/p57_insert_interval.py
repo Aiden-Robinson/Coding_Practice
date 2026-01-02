3f7c987fed1c8f29bbb34b4bb8e13f999a2139fb
Aiden Robinson <aiden.robinson1@gmail.com>
2026-01-02 15:50:28 -0800

    Refactor insert method in p57_insert_interval.py
    Refactor insert method to improve readability and logic.

--- Intervals/p57_insert_interval.py
+++ Intervals/p57_insert_interval.py
@@ -1,22 +1,21 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res= []
-
+        
+        res=[]
         for i in range(len(intervals)):
-            if newInterval[1]<intervals[i][0]: #completely before it
+            if newInterval[1]<intervals[i][0]: #completely before
                 res.append(newInterval)
                 return res + intervals[i:]
-            elif newInterval[0]>intervals[i][1]: #completely after it
+            elif newInterval[0]>intervals[i][1]: #completely after
                 res.append(intervals[i])
             
-
-            else: #merge
-
-                newInterval= [min(newInterval[0],intervals[i][0]),max(newInterval[1],intervals[i][1])]
-        
-        res.append(newInterval)
+            else: #me
+                newInterval= (min(intervals[i][0],newInterval[0]),max(intervals[i][1],newInterval[1]))
             
+        res.append(newInterval)
+
         return res
+
 
 
     #time complexity O(N)
